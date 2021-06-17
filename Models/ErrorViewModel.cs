d2ebed83ca98f0db09ada8456ad644da027e580f
Rathinam <rathinam@zilliontechnologies.com>
2021-06-17 22:43:16 +0530

    Initial check-in

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DashJenkinCiDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
