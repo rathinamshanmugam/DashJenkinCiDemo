da58690a3d8b8183203871a21fc37ce011bf8a8a
Rathinam <rathinam@zilliontechnologies.com>
2021-06-19 21:39:11 +0530

    testing

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace DashJenkinCiDemo.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
+        public string Forename { get; set; }
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
